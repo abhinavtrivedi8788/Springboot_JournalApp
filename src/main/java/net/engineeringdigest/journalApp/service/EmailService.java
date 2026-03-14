c0954a6f045c04b6fe8c9390630ed425f0c348c5
Abhinav Trivedi <abhinavtrivedii@outlook.com>
2026-03-14 13:54:32 +0530

    Email

--- src/main/java/net/engineeringdigest/journalApp/service/EmailService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/EmailService.java
@@ -14,7 +14,7 @@ public class EmailService {
 
     public void sendEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("dixit.10@gmail.com");
+        message.setFrom("from_mail");
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
